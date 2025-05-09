aa1c830535a87bbb757b3ba968af5a9cce1a0651
mhdna <mhdna@outlook.com>
2025-05-09 20:22:48 +0300

    Cleanup summaries so that it is in its own component

--- webui/src/components/Summaries.vue
+++ webui/src/components/Summaries.vue
@@ -1,106 +1,69 @@
 <!-- TODO add dynamic numbers (See: https://www.7te.net/zp-ui/#/dynamicnumbers) -->
 <template>
-  <v-container max-width="1400" class="d-flex justify-space-between px-10">
-    <v-card class="text-center mr-2" style="border-bottom: 6px solid #5c6bc0">
-      <v-card-item class="d-flex">
-        <div class="d-flex justify-space-between">
-          <div class="text-no-wrap"># Successful Requests</div>
-          <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" />
-        </div>
-        <div class="d-flex">
-          <span class="text-h3 my-1">10,230</span>
-          <span>req</span>
-        </div>
-      </v-card-item>
-    </v-card>
-    <v-card class="text-center mr-2" style="border-bottom: 6px solid #5c6bc0">
-      <v-card-item>
-        <div class="d-flex justify-space-between">
-          <div class="text-no-wrap"># Of Requests Banned</div>
-          <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" />
-        </div>
-        <div class="d-flex">
-          <span class="text-h3 my-1">3,120</span>
-          <span>req</span>
-        </div>
-      </v-card-item>
-    </v-card>
-    <v-card class="text-center mr-2" style="border-bottom: 6px solid #5c6bc0">
-      <v-card-item>
-        <div class="d-flex justify-space-between">
-          <div class="text-no-wrap">Weekly Activity</div>
-          <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" />
-        </div>
-        <div class="d-flex">
-          <span class="text-h3 my-1">75</span>
-          <span>%</span>
-          <span class="d-flex align-center ga-2 text-green-darken-4">
-            <v-avatar variant="tonal" size="24" icon="mdi-arrow-up">
-              <v-icon size="16" />
-            </v-avatar>
-            +9%
-          </span>
-        </div>
-      </v-card-item>
-    </v-card>
-    <v-card class="text-center mr-2" style="border-bottom: 6px solid #5c6bc0">
-      <v-card-item>
-        <div class="d-flex justify-space-between">
-          <div class="text-no-wrap">Today's Activity</div>
-          <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" />
-        </div>
-        <div class="d-flex">
-          <span class="text-h3 my-1">23</span>
-          <span>%</span>
-          <span class="d-flex align-center ga-2 text-red-darken-4">
-            <v-avatar variant="tonal" size="24" icon="mdi-arrow-down">
-              <v-icon size="16" />
-            </v-avatar>
-            -13%
-          </span>
-        </div>
-      </v-card-item>
-    </v-card>
-    <v-card class="text-center mr-2" style="border-bottom: 6px solid #5c6bc0">
-      <v-card-item>
-        <div class="d-flex justify-space-between">
-          <div class="text-no-wrap">Projects</div>
-          <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" />
-        </div>
-        <div class="d-flex">
-          <span class="text-h3 my-1">2</span>
-          <span class="d-flex align-center ga-2 text-red-darken-4">
-            <v-avatar variant="tonal" size="24" icon="mdi-arrow-down">
-              <v-icon size="16" />
-            </v-avatar>
-            -1
-          </span>
-        </div>
-      </v-card-item>
-    </v-card>
-    <v-card class="text-center mr-2" style="border-bottom: 6px solid #5c6bc0">
-      <v-card-item>
-        <div class="d-flex justify-space-between">
-          <div class="text-no-wrap">Productive Time</div>
-          <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" />
-        </div>
-        <div class="d-flex">
-          <span class="text-h3 my-1"> 131:142:11 </span>
-          <span>hours</span>
-          <span class="d-flex align-center ga-2 text-red-darken-4">
-            <v-avatar variant="tonal" size="24" icon="mdi-arrow-down">
-              <v-icon size="16" />
-            </v-avatar>
-            +1:40:02
-          </span>
-        </div>
-      </v-card-item>
-    </v-card>
+  <v-container
+    class="d-flex overflow-x-scroll justify-start"
+    max-width="94%"
+    id="summaries-card"
+  >
+    <SummaryCard
+      min-width="203"
+      title="# Of Successful Requests"
+      count="10,230"
+      unit="req"
+    />
+
+    <SummaryCard
+      min-width="180"
+      title="# Of Requests Banned"
+      count="3,120"
+      unit="req"
+    />
+
+    <SummaryCard
+      min-width="150"
+      title="Weekly Activity"
+      count="75"
+      unit="%"
+      change-rate="9"
+      change-rate-unit="%"
+    />
+    <SummaryCard
+      min-width="154"
+      title="Today's Activity"
+      count="23"
+      unit="%"
+      change-rate="-9"
+      changeRateIsNegative="true"
+      change-rate-unit="%"
+    />
+    <SummaryCard
+      min-width="126"
+      title="Projects"
+      count="2"
+      unit="%"
+      change-rate="-10"
+      changeRateIsNegative="true"
+    />
+    <SummaryCard
+      min-width="425"
+      title="Productive Time"
+      count="131:142:43"
+      unit="hours"
+      change-rate="131:142:43"
+    />
+    <!-- -->
+    <!-- -->
+    <!-- req -->
+
+    <!-- style="border-bottom: 6px solid #5c6bc0" -->
   </v-container>
+
   <!-- <v-container class="d-flex justify-space-between"> -->
-  <v-row class="mb-4 mt-1">
+
+  <div class="mt-4">
     <ActiveUsersListHorizontal />
-  </v-row>
+  </div>
+
   <!-- </v-container> -->
 </template>
 
---
+++ webui/src/components/SummaryCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-card
+    class="d-flex flex-column justify-center me-2 position-relative"
+    :min-width="minWidth"
+  >
+    <!-- style="border-bottom: 6px solid #5c6bc0" -->
+    <div
+      class="d-flex position-absolute top-0 left-0 right-0 bg-surface-light align-center px-1"
+      style="height: 16px; background-color: grey"
+    >
+      <v-icon class="cursor-surface" icon="mdi-drag-horizontal" />
+      <!-- <v-spacer /> -->
+      <!-- <v-icon class="cursor-surface" icon="mdi-close" /> -->
+    </div>
+    <div
+      class="text-center bg-surface pt-1 mt-3 mb-1"
+      style="height: 15px; background-color: grey"
+    >
+      {{ title }}
+    </div>
+    <v-card-item class="d-flex justify-center">
+      <!-- <div class="d-flex justify-center"> -->
+      <!-- <div class="text-no-wrap"># Of Successful Requests</div> -->
+      <!-- <v-icon color="grey" class="me-3 cursor-grab" icon="mdi-drag" /> -->
+      <!-- </div> -->
+      <div class="d-flex">
+        <span class="text-h3 my-1">{{ count }}</span>
+        <span>{{ unit }}</span>
+        <span v-if="changeRate" class="d-flex justify-center">
+          <span
+            v-if="changeRateIsNegative"
+            class="d-flex align-center ga-2 text-red-darken-4"
+          >
+            <v-avatar variant="tonal" size="24" icon="mdi-arrow-down">
+              <v-icon size="16" />
+            </v-avatar>
+            {{ changeRate }}{{ changeRateUnit }}
+          </span>
+          <span v-else class="d-flex align-center ga-2 text-green-darken-4">
+            <v-avatar variant="tonal" size="24" icon="mdi-arrow-up">
+              <v-icon size="16" />
+            </v-avatar>
+            {{ changeRate }}{{ changeRateUnit }}
+          </span>
+        </span>
+      </div>
+    </v-card-item>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    minWidth: number;
+    title: string;
+    count: number;
+    unit: string;
+    changeRate?: string;
+    changeRateIsNegative?: boolean;
+    changeRateUnit?: string;
+  }>(),
+  {
+    changeRateIsNegative: false,
+    unit: "",
+    changeRate: 0,
+  },
+);
+</script>
