6a014e402b95cc313813342fd8f1eb7f36053d60
mhdna <mhdna@outlook.com>
2025-05-12 22:43:44 +0300

    Trying out vuejs-tour

---
+++ webui/src/components/AppTour.vue
@@ -0,0 +1,20 @@
+<template>
+  <VTour :steps="steps" autoStart />
+</template>
+
+<script setup lang="ts">
+import { VTour } from "@globalhive/vuejs-tour";
+import "@globalhive/vuejs-tour/dist/style.css";
+
+const steps = [
+  {
+    target: "#login_card",
+    content:
+      "Here you can see the summaries of current events with how much growth or drop.",
+  },
+  {
+    target: "#live_preview",
+    content: "Step 2",
+  },
+];
+</script>
