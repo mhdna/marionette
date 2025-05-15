feed55cafda5c8d5ec9d44531ef2d70abf40ce4a
mhdna <mhdna@outlook.com>
2025-05-15 16:38:28 +0300

    Turn websites list into a data-table

--- webui/src/components/cards/WebsitesList.vue
+++ webui/src/components/cards/WebsitesList.vue
@@ -1,61 +1,33 @@
 <template>
-  <v-card title="Most visited Domain names" flat>
-    <div v-for="(domain, index) in domains" :key="index" class="px-4">
-      <v-card
-        hover
-        flat
-        rounded="lg"
-        class="px-10 py-1 mb-2 d-flex justify-space-between align-center"
-      >
-        <div style="width: 6%">
-          <v-chip
-            density="compact"
-            class="py-0 px-2 me-4 d-flex justify-center"
-            >{{ index + 1 }}</v-chip
-          >
-        </div>
-        <div style="width: 50%">
-          {{ domain }}
-        </div>
-        <v-chip density="compact" class="py-0 me-2">800 hits</v-chip>
-        <v-progress-linear
-          style="width: 40%"
-          width="80"
-          buffer-opacity="1"
-          buffer-value="3"
-          color="red-darken-4"
-          max="100"
-          min="1"
-          model-value="66"
-          rounded
-          height="10"
-        >
-          <!-- buffer-color="#ff0000" -->
-          <!-- <strong>{{ 28 }}%</strong> -->
-        </v-progress-linear>
-      </v-card>
-    </div>
-    <v-pagination rounded density="compact"></v-pagination>
-    <!-- v-model="page" -->
-    <!-- :length="pageCount" -->
+  <v-card id="websites-list" flat>
+    <v-divider></v-divider>
+    <v-data-table
+      density="compact"
+      v-model:search="search"
+      :filter-keys="['name']"
+      :items="items"
+      items-per-page="10"
+      :hide-default-header="true"
+      :hide-default-footer="true"
+    >
+    </v-data-table>
   </v-card>
 </template>
-
-<script setup lang="ts">
-const domains = [
-  "google.com",
-  "youtube.com",
-  "facebook.com",
-  "amazon.com",
-  "wikipedia.org",
-  "twitter.com",
-  "instagram.com",
-  "linkedin.com",
-  "netflix.com",
-  "yahoo.com",
-];
-
+<script setup>
 import { ref } from "vue";
 
-const value = ref(28);
+const search = ref("");
+
+const items = [
+  { name: "google.com", hits: 900 },
+  { name: "youtube.com", hits: 200 },
+  { name: "facebook.com", hits: 400 },
+  { name: "amazon.com", hits: 350 },
+  { name: "wikipedia.org", hits: 325 },
+  { name: "twitter.com", hits: 342 },
+  { name: "instagram.com", hits: 124 },
+  { name: "linkedin.com", hits: 32 },
+  { name: "netflix.com", hits: 12 },
+  { name: "yahoo.com", hits: 2 },
+];
 </script>
