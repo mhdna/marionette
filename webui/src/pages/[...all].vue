8105e167ffc20f10e1334548dc85ed2836ae1563
mhdna <mhdna@outlook.com>
2025-05-15 05:18:00 +0300

    headline as a template inside of the 404 page

--- webui/src/pages/[...all].vue
+++ webui/src/pages/[...all].vue
@@ -4,6 +4,13 @@
     title="Page not found"
     text="The page you were looking for does not exist"
   >
+    <template v-slot:headline>
+      <div class="d-flex flex-column justify-center align-center">
+        <SadDot size="120" class="mb-4" />
+        <!-- foce ltr so that the face is not flipped -->
+        <div>Whoops, 404</div>
+      </div>
+    </template>
     <!-- <v-img src="/logo.png" width="400"> </v-img> -->
   </v-empty-state>
 </template>
