9cf6707d25e4d5a1bd579b4a106c0be2fcabed52
mhdna <mhdna@outlook.com>
2025-05-17 09:19:45 +0300

    feat: Add navigation links component

---
+++ webui/src/components/NavigationLinks.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-flex align-center">
+    <v-icon
+      icon="mdi-chevron-right"
+      :class="`mx-2 me-0 ${isRtl ? 'flip-in-rtl' : ''}`"
+    />
+    <v-breadcrumbs :items="items"> </v-breadcrumbs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+  isRtl: boolean;
+}>();
+
+const items = ref([
+  {
+    title: "Dashboard",
+    disabled: false,
+    href: "breadcrumbs_dashboard",
+  },
+  {
+    title: "Link 1",
+    disabled: false,
+    href: "breadcrumbs_link_1",
+  },
+]);
+</script>
