5f80bbc7a4dbf81c67041b4a935f0f6bdf9e93a1
mhdna <mhdna@outlook.com>
2025-05-17 16:37:07 +0300

    Card toolbar allow slots

--- webui/src/components/cards/CardToolbar.vue
+++ webui/src/components/cards/CardToolbar.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="pt-2 px-4 pb-2 mb-4 font-weight-bold bg-surface-light">
-    {{ title }}
+  <div
+    class="pt-2 px-4 pb-2 mb-4 font-weight-bold bg-surface-light d-flex justify-space-between align-center"
+  >
+    <div>
+      {{ title }}
+    </div>
+    <div>
+      <slot />
+    </div>
   </div>
 </template>
 
