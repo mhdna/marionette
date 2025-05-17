f47f587f85b56e081ef1c5b772e50816ad6157ee
mhdna <mhdna@outlook.com>
2025-05-17 18:30:05 +0300

    shorter height

--- webui/src/components/cards/ActionCard.vue
+++ webui/src/components/cards/ActionCard.vue
@@ -4,10 +4,10 @@
     border="sm"
     link
     hover
-    height="90"
+    height="60"
     class="d-flex flex-1 justify-center px-8 align-center w-100"
   >
-    <v-icon :icon="props.icon" size="45" class="me-8" />
+    <v-icon :icon="props.icon" size="35" class="me-8" />
     <div class="text-h6 d-flex justify-center align-center">
       {{ props.title }}
     </div>
