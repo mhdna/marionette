2cdf11d339c89c8f0c0fe92b4ca8b52796f8f1a2
mhdna <mhdna@outlook.com>
2025-05-18 15:04:11 +0300

    Cleaner Requests Page

--- webui/src/pages/dashboard/internet_usage.vue
+++ webui/src/pages/dashboard/internet_usage.vue
@@ -1,60 +1,13 @@
 <template>
-  <v-row no-gutters class="mt-2">
-    <v-col :md="12" cols="12 " :sm="12" class="pa-1">
-      <v-card class="mx-4" rounded>
-        <v-toolbar class="px-4" height="54">
-          <h2 class="text-h6">{{ $t("cards.requests") }}</h2>
-        </v-toolbar>
-        <!-- <BarChart /> -->
-
-        <!-- <DateTimeChart /> -->
-        <!-- <EAreaChart /> -->
-        <!-- <ELineChart /> -->
-        <v-card-text>
+  <v-container fluid max-width="1400">
+    <RoundedCard class="me-0 me-2 mb-2">
+      <div>
+        <CardToolbar title="Requests Rate" />
+        <div class="px-4">
           <ELineChart2 />
-        </v-card-text>
-        <!-- <BarChart /> -->
-        <!-- <Heatmap /> -->
-        <!-- <DateTimeChart /> -->
-        <!-- <line-chart -->
-        <!--   :data="[ -->
-        <!--     { -->
-        <!--       name: 'Workout', -->
-        <!--       data: { -->
-        <!--         '2025-01-01 00:00:00 -0800': 3, -->
-        <!--         '2025-01-02 00:00:00 -0800': 4, -->
-        <!--       }, -->
-        <!--     }, -->
-        <!--     { -->
-        <!--       name: 'Call parents', -->
-        <!--       data: { -->
-        <!--         '2025-01-01 00:00:00 -0800': 5, -->
-        <!--         '2025-01-02 00:00:00 -0800': 3, -->
-        <!--       }, -->
-        <!--     }, -->
-        <!--   ]" -->
-        <!-- /> -->
-      </v-card>
-
-      <!-- <v-card rounded class="d-flex justify-space-between" height="690"> -->
-    </v-col>
-  </v-row>
-  <LoadingTable />
-
-  <div class="d-flex mb-4">
-    <RoundedCard class="mx-4 me-0">
-      <CardToolbar title="Most Visited Categories" />
-      <div class="px-4 pb-4 flex-md-grow-1">
-        <EPieChart />
-        <WebsitesList />
+          <LoadingTable />
+        </div>
       </div>
     </RoundedCard>
-    <RoundedCard class="mx-4">
-      <CardToolbar title="Most Banned Categories" />
-      <div class="px-4 flex-md-grow-1">
-        <EPieChart />
-        <WebsitesList />
-      </div>
-    </RoundedCard>
-  </div>
+  </v-container>
 </template>
